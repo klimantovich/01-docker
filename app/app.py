bbea1e72d21d9f555611fa86b511a075c9648519
vitali <vitali@gmail.com>
2023-12-30 13:50:35 +0300

    pass sensetive data securely

--- app/app.py
+++ app/app.py
@@ -8,9 +8,9 @@ mysql = MySQL()
 mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or 'localhost'
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'passw0rd'
-app.config['MYSQL_DATABASE_DB'] = 'employee_db'
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE']
 app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
 mysql.init_app(app)
 
